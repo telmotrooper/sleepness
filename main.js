a9aafe8c5f448220e45126109cc8628771b3901a
Telmo Valverde <telmo.valverde@tarmac.io>
2023-12-11 11:25:42 -0300

    Rendering a page

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log('Hello')
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
