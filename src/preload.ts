cea864e177e61c3514e76c7567ddd56a66180a5b
Telmo Valverde <telmo.valverde@tarmac.io>
2023-12-22 21:27:41 -0300

    Initialized fresh project with Electron Forge

--- src/preload.ts
+++ src/preload.ts
@@ -1,12 +1,2 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const replaceText = (selector: string, text: string) => {
-    const element = document.getElementById(selector)
-    if (element) {
-      element.innerText = text
-    }
-  }
-
-  for (const dependency of ["chrome", "node", "electron"]) {
-    replaceText(`${dependency}-version`, process.versions[dependency])
-  }
-})
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
